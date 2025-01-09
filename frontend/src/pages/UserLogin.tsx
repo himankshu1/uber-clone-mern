916c5d29e5e5820e3e50c1e70fa2e0cc69b02b2a
Himanshu Kumar <himankshu1@gmail.com>
2025-01-09 14:48:41 +0530

    user form logic

--- frontend/src/pages/UserLogin.tsx
+++ frontend/src/pages/UserLogin.tsx
@@ -1,15 +1,39 @@
+import { FormEvent, useState } from "react";
 import { Link } from "react-router";
 
+type FormData = {
+    email: string;
+    password: string;
+};
+
 export default function UserLogin() {
+    const [formData, setFormData] = useState<FormData>({
+        email: "",
+        password: "",
+    });
+
+    const submitHandler = (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log(formData);
+    };
     return (
         <div className="p-7 flex flex-col justify-between h-screen">
-            <form>
+            <form onSubmit={submitHandler}>
                 <h3 className="text-xl mb-2">What's your email?</h3>
                 <input
                     required
                     type="email"
                     placeholder="email@example.com"
                     className="bg-zinc-100 mb-7 border rounded px-4 py-2 w-full text-lg placeholder:text-base"
+                    name="email"
+                    value={formData.email}
+                    onChange={(e) =>
+                        setFormData({
+                            ...formData,
+                            [e.target.name]: e.target.value,
+                        })
+                    }
                 />
 
                 <h3 className="text-xl mb-2">Enter Password</h3>
@@ -18,6 +42,14 @@ export default function UserLogin() {
                     type="password"
                     placeholder="*******"
                     className="bg-zinc-100 mb-7 border rounded px-4 py-2 w-full text-lg placeholder:text-base"
+                    name="password"
+                    value={formData.password}
+                    onChange={(e) =>
+                        setFormData({
+                            ...formData,
+                            [e.target.name]: e.target.value,
+                        })
+                    }
                 />
 
                 <button className="bg-black text-white px-6 py-4 w-full font-medium mt-2 mb-4">
@@ -33,9 +65,12 @@ export default function UserLogin() {
             </form>
 
             <div>
-                <button className="bg-[#FCDF29] px-6 py-4 w-full font-medium mt-2">
+                <Link
+                    to="/captain-login"
+                    className="bg-[#FCDF29] px-6 py-4 w-full font-medium my-5 flex items-center justify-center"
+                >
                     Sign in as Captain
-                </button>
+                </Link>
             </div>
         </div>
     );
